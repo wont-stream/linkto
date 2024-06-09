5ccdd4ca0d88484aaf889a7b8ec90c6157e9c42a
Unstream <143244075+wont-stream@users.noreply.github.com>
2024-06-09 15:28:10 -0400

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,31 @@
+const makeHTML = (link) => {
+    const encodedLink = encodeURI(link);
+    return `<!DOCTYPE html><html lang="en"><head><meta http-equiv="refresh" content="0;url=${encodedLink}"></head><body><script type="text/javascript">window.location.href="${encodedLink}";</script></body></html>`;
+}
+
 export default {
-  async fetch(request, env) {
-    const url = new URL(request.url);
-    if (url.pathname.startsWith('/mail/')) {
-      // TODO: Add your custom /api/* logic here.
-      return new Response.redirect(url.pathname.replace("/mail/", ""), 301);
-    }
-    // Otherwise, serve the static assets.
-    // Without this, the Worker will error and no assets will be served.
-    return env.ASSETS.fetch(request);
-  },
+    async fetch(request, env) {
+        const url = new URL(request.url);
+        let html;
+
+        const sanitizeAndMakeHTML = (prefix, protocol) => {
+            if (url.pathname.startsWith(prefix)) {
+                const targetBase = `${protocol}:`;
+                const targetParams = new URLSearchParams(url.search);
+                const target = targetBase + encodeURIComponent(url.pathname.replace(prefix, '')) + (targetParams.toString() ? `?${targetParams.toString()}` : '');
+                return makeHTML(target);
+            }
+            return null;
+        }
+
+        html = sanitizeAndMakeHTML('/mail/', 'mailto');
+        if (!html) html = sanitizeAndMakeHTML('/sms/', 'sms');
+        if (!html) html = sanitizeAndMakeHTML('/tel/', 'tel');
+
+        if (!html) {
+            return new Response('Not Found', { status: 404 });
+        }
+
+        return new Response(html, { headers: { 'Content-Type': 'text/html' } });
+    },
 }
