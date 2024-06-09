589a1d847cec817dfa610ab91f8da607f6045feb
Unstream <143244075+wont-stream@users.noreply.github.com>
2024-06-09 15:17:25 -0400

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,9 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
-    if (url.pathname.startsWith('/api/')) {
+    if (url.pathname.startsWith('/mail/')) {
       // TODO: Add your custom /api/* logic here.
-      return new Response('Ok');
+      return new Response.redirect(url.pathname.replace("/mail/", ""), 301);
     }
     // Otherwise, serve the static assets.
     // Without this, the Worker will error and no assets will be served.
